e990a728c19bd8d2448f57eba964f50c4be9416f
A-1515-N <107986538+A-1515-N@users.noreply.github.com>
2022-11-08 10:51:07 +0900

    Initial commit

---
+++ PHP2_2-7/index.php
@@ -0,0 +1,11 @@
+<!-- 【2章 課題】配列、連想配列 -->
+<?php
+$colours = ["red" => "赤", "blue" => "青", "green" => "緑"];
+
+var_dump($colours);
+echo '<br>';
+
+$colours["yellow"] = "黄色";
+var_dump($colours);
+
+?>
